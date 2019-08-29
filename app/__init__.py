8f291ecc6d02a06d713c09e4086451938f4f0466
Jennifer Young <jtyoung@Jennifers-MBP-3.ucsf.edu>
2019-08-29 14:30:05 -0700

    refactor error handling

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,7 @@ login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
