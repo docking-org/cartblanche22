ebd94386876996d0093458fe2fa84a696aca5b96
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2020-02-13 15:57:21 -0800

    punchout first test finished

--- app/main/punchout.py
+++ app/main/punchout.py
@@ -1,7 +1,15 @@
-from flask import Response, request
-from flask_login import current_user
+from flask import Response, request, current_app, redirect, jsonify, url_for
+from flask_login import current_user, login_user
 from app.main import application
 import xmltodict
+from app.data.models.users import Users
+from time import time
+import jwt
+from app import db
+import datetime
+import string
+import random
+
 
 @application.route('/punchoutOrder', methods= ['GET'])
 def punchoutOrder():
@@ -45,20 +53,67 @@ timestamp='2004-06-14T12:59:09-07:00'>
 def punchoutSetup():
     xml_data = request.get_data()
     content_dict = xmltodict.parse(xml_data)
-    print(content_dict['cXML']['Header']['From']['Credential']['@domain'])
-    data = '''
-    <!DOCTYPE cXML SYSTEM "http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd">
-<cXML payloadID="958074700772@www.workchairs.com" timestamp="2005-06-14T12:59:09-07:00">
-<Response>
-<Status code="200" text="success"/>
-<PunchOutSetupResponse>
-<StartPage>
-<URL>http://cartblanche.docking.org</URL>
-</StartPage>
-</PunchOutSetupResponse>
-</Response>
-</cXML>
-'''
-
+    cookie = content_dict['cXML']['Request']['PunchOutSetupRequest']['BuyerCookie']
+    url = content_dict['cXML']['Request']['PunchOutSetupRequest']['SupplierSetup']['URL']
+    identity = content_dict['cXML']['Header']['Sender']['Credential']['Identity']
+    password = content_dict['cXML']['Header']['Sender']['Credential']['SharedSecret']
+    print(password)
+    print(cookie, url)
+    randStr = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+    data = genProlog("1.0", randStr)
+    user = Users.query.filter_by(username=identity).first()
+    if user is None or not user.check_password(password):
+        print('Invalid username or password')
+        data += '''
+        <Response>
+        <Status code="400" Text="Bad Request">Invalid Document. Something wrong with finding associated user.</Status>
+        </Response>
+        </cXML>'''
+        return Response(data, mimetype='text/xml')
+    print(user)
+    token = jwt.encode(
+            {'user_id': user.id, 'exp': time() + 1800},
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+    print(token)
+    data += '''
+    <Response>
+    <Status code="200" text="OK"/>
+    <PunchOutSetupResponse>
+    <StartPage>
+    <URL>https://cartblanche.docking.org/punchoutStart/
+    '''
+    data += token
+    data += '''
+    </URL>
+    </StartPage>
+    </PunchOutSetupResponse>
+    </Response>
+    </cXML>
+    '''
     return Response(data, mimetype='text/xml')
     
+@application.route('/punchoutStart/<token>', methods= ['GET', 'POST'])
+def punchoutStart(token):
+    # # decoding token
+    print(token)
+    try:
+        user_id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['user_id']
+        user = Users.query.get(user_id)
+        login_user(user)
+        print(current_user)
+        return redirect(url_for('main.sw'))
+    except:
+        return jsonify('something wrong with starting the page, token expired or user not found')
+
+def genProlog(cXMLvers, randStr):
+    vers = "1.2.014";
+    sysID = "http://xml.cXML.org/schemas/cXML/" + vers + "/cXML.dtd";
+    dt = datetime.datetime.now()
+    nowNum = int(round(time()*1000))
+    timeStr = dt.strftime("%y-%m-%dT%H:%M:%S")
+    data = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    data += '<!DOCTYPE cXML SYSTEM "' + sysID + '">\n';
+    data += '<cXML payloadID="' + str(nowNum) + ".";
+    data += randStr + '@' + 'www.cartblanche.docking.org';
+    data += '" timestamp="' + timeStr + '">';
+    return data;
