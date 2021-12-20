fe3b99824595c9d67de1c9bbc43988fb9687d70e
Jose <josecastanon4@gmail.com>
2021-12-19 22:58:23 -0800

    updated boot script

--- boot.sh
+++ boot.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-exec gunicorn -b :5000 -w 5 --access-logfile - --error-logfile - application:application --timeout 0 &
-exec redis-server & 
-exec celery -A app.data.tasks.search_zinc worker -l INFO --autoscale=20,3 & 
+exec supervisord -c supervisord.conf
 
 
-fg %1
