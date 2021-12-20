5cbfe8e39d07a6597656219ea187016fdf67dff9
Jose <josecastanon4@gmail.com>
2021-12-19 23:14:20 -0800

    removed supervisor

--- boot.sh
+++ boot.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-exec supervisord -c supervisord.conf
+#!/bin/bash
+  
+set -m
+
+gunicorn -b :5000 -w 5 --access-logfile - --error-logfile - application:application --timeout 0 &
+redis-server &
+celery -A app.data.tasks.search_zinc worker -l INFO
+  
+fg %1
 
 
