cf31bfe16202991d9b0f357154f96a3e1af117a4
Jose <josecastanon4@gmail.com>
2021-12-19 23:49:52 -0800

    updated requirements

--- boot.sh
+++ boot.sh
@@ -3,7 +3,6 @@
 set -m
 
 gunicorn -b :5000 -w 5 --access-logfile - --error-logfile - application:application --timeout 0 &
-redis-server &
 celery -A app.data.tasks.search_zinc worker -l INFO
   
 fg %1
