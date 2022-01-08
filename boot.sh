ba01a257805426ad4ee2f80d090b877df7e36bfd
Jose <josecastanon4@gmail.com>
2022-01-07 16:50:01 -0800

    updated 3d tranchebrowser to show generation

--- boot.sh
+++ boot.sh
@@ -2,8 +2,8 @@
   
 set -m
 
-gunicorn -b :5000 -w 5 --access-logfile - --error-logfile - application:application --timeout 0 &
-celery -A app.data.tasks.search_zinc worker -l INFO
+gunicorn -b :5000 -w 1 --access-logfile - --error-logfile - application:application --timeout 0 --reload &
+celery -A app.data.tasks.search_zinc worker -l INFO --autoscale=6,4
   
 fg %1
 
