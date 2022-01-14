4f58911b4bff2cf7f1981dc48adc79b34ab4fc08
Jose <josecastanon4@gmail.com>
2022-01-14 12:53:52 -0800

    updated tranche browser, zincid search

--- boot.sh
+++ boot.sh
@@ -2,8 +2,8 @@
   
 set -m
 
-gunicorn -b :5000 -w 1 --access-logfile - --error-logfile - application:application --timeout 0 --reload &
-celery -A app.data.tasks.search_zinc worker -l INFO --autoscale=6,4
+gunicorn -b :5000 -w 2 --access-logfile - --error-logfile - application:application --timeout 0 --reload &
+celery -A app.data.tasks.search_zinc worker -l INFO --autoscale=20,10
   
 fg %1
 
