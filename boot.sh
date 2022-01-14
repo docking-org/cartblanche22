c1642f13b6860783022af2660d35c8e15af23c0b
Jose <josecastanon4@gmail.com>
2022-01-14 13:29:50 -0800

    updated boot script

--- boot.sh
+++ boot.sh
@@ -2,8 +2,8 @@
   
 set -m
 
-gunicorn -b :5000 -w 2 --access-logfile - --error-logfile - application:application --timeout 0 --reload &
-celery -A app.data.tasks.search_zinc worker -l INFO --autoscale=20,10
+gunicorn -b :5000 -w 6 --access-logfile - --error-logfile - application:application --timeout 0 --reload &
+celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=20
   
 fg %1
 
