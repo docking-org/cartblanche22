0a4f190e5b9a7f9228b92145de772d846823c905
Jose <josecastanon4@gmail.com>
2022-01-24 01:56:41 -0800

    fixed error

--- boot.sh
+++ boot.sh
@@ -2,9 +2,7 @@
   
 set -m
 
-redis-server &
 celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=10 &
-celery -A app.data.tasks.search_zinc flower --address=127.0.0.1 --port=5002 --broker=redis://localhost:6379/0 --result-backend=redis://localhost:6379/0 &
 gunicorn  -b :5000 -w 5 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 0 --reload
   
 fg %1
