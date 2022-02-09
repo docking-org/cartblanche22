4f35fc4c8f8e1a672619990a28a7f44cf5353353
s_btingle <s_btingle@n-1-17.cluster.ucsf.bkslab.org>
2022-02-09 14:52:33 -0800

    updated boot config for debug

--- boot.sh
+++ boot.sh
@@ -2,7 +2,8 @@
   
 set -m
 
-celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=30 &
+mkdir -p /tmp/logs
+celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=30 --logfile=/tmp/logs/workerlog &
 gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 10 --reload
   
 fg %1
