4a9e9c7900da3aa1354ad0986297027f0e2de67b
root <root@n-1-17.cluster.ucsf.bkslab.org>
2022-02-14 16:56:39 -0800

    autocommit Mon Feb 14 16:56:39 PST 2022

--- app/data/tasks/search_smiles.py
+++ app/data/tasks/search_smiles.py
@@ -46,7 +46,7 @@ class SearchSmiles(Resource):
         textDataList = [x for x in re.split(' |, |,|\n, |\r, |\r\n', data) if x!='']
         fileDataList = file.split('\n')
 
-	allSmiles = [smi for smi in textDataList + fileDataList if smi != '']
+        allSmiles = [smi for smi in textDataList + fileDataList if smi != '']
         files = {
             'smiles-in': ','.join(allSmiles),
             'dist': dist,
