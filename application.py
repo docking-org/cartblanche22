a044c40152ad73c071851e8650658b5823a18a1e
root <root@n-1-17.cluster.ucsf.bkslab.org>
2022-02-15 10:45:36 -0800

    autocommit Tue Feb 15 10:45:36 PST 2022

--- application.py
+++ application.py
@@ -3,10 +3,10 @@ from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    # from gevent import monkey
+    from gevent import monkey
     # from psycogreen.gevent import patch_psycopg
     #
-    # monkey.patch_all(subprocess=True, ssl=False)
+    monkey.patch_all(subprocess=True, ssl=False)
     # patch_psycopg()
     # application.debug = True
     print("before application run")
