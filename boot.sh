184054bba24041cbcc0504abd2d6885aa400e86a
jose <josecastanon4@gmail.com>
2022-05-09 23:31:13 -0700

    added long search progress
    Former-commit-id: 3d15c68b032a5d4f839165e1fd50440d9148b7c2

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
   
 set -m
 
-celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=30 &
-gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 30 --reload
+celery -A app.data.tasks.search_zinc worker -l INFO --concurrency=120 &
+gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 300 --reload
   
 fg %1
