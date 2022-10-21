1f19ebadb137b33a71cd236432f4d99ece016925
jose <josecastanon4@gmail.com>
2022-10-21 16:33:18 -0700

    zincsearch bug fix

--- app/main/search.py
+++ app/main/search.py
@@ -229,7 +229,7 @@ def searchZinc20(identifier):
         return render_template('molecule/mol_index.html', data=data, prices=prices,
                                smile=urllib.parse.quote(smile), response=res, identifier=identifier, zinc20_stock='zinc20_stock')
     else:
-        return render_template('errors/search404.html', lines=files, href='/search/zincid',
+        return render_template('errors/search404.html', lines=[identifier], href='/search/zincid',
                                header="We didn't find this molecule from Zinc22 database. Click here to return"), 404
 
 @application.route('/searchZinc/<identifier>')
