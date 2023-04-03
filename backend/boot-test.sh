fa4636364fcf0039a902410670365dad617c9d77
s_jcastanon <s_jcastanon@epyc2.cluster.ucsf.bkslab.org>
2023-04-03 08:59:30 -0700

    fixed boot.sh

--- backend/boot-test.sh
+++ backend/boot-test.sh
@@ -3,7 +3,7 @@
 set -m
 
 rabbitmq-server -detached &
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker3 --concurrency=5
+celery -A cartblanche.run_celery.celery worker -l INFO -n worker3 --concurrency=5 &
 celery -A cartblanche.run_celery.celery worker flower &
 gunicorn  -b :5000 -w 3 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload
 
--- backend/boot.sh
+++ backend/boot.sh
@@ -3,7 +3,7 @@
 set -m
 
 rabbitmq-server -detached &
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker
+celery -A cartblanche.run_celery.celery worker -l INFO -n worker &
 celery -A cartblanche.run_celery.celery worker flower &
 gunicorn  -b :5000 -w 40 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload
 
