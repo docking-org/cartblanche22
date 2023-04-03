03861ddcc388bd910bb1c5fe2ee4aff39feabc57
s_jcastanon <s_jcastanon@epyc2.cluster.ucsf.bkslab.org>
2023-04-03 13:37:31 -0700

    fixing 'find similar' button not showing correct smiles in url

--- src/Substance/Substance.jsx
+++ src/Substance/Substance.jsx
@@ -178,7 +178,7 @@ export default function Substance() {
                                     }}>
                                         <ButtonGroup>
                                             <Button variant="warning"
-                                                href={"/similarity/sw?smiles=" + results.smiles}
+                                                href={"/similarity/sw?smiles=" + encodeURIComponent(results.smiles)}
                                             >
                                                 Find Similar
                                             </Button>
