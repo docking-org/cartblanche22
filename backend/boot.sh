8bdbcc7b239eab65aa3d27d0dc08ed76f199dedd
Mar Castanon <josecastanon4@gmail.com>
2023-07-20 19:44:19 +0000

    Update boot.sh

--- backend/boot.sh
+++ backend/boot.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -m
 
 rabbitmq-server -detached &
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker &
+celery -A cartblanche.run_celery.celery worker -l INFO -n worker --max-tasks-per-child=1 &
 celery -A cartblanche.run_celery.celery flower &
-gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload
+gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload --max-requests 3
 
 fg %1
