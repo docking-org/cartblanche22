423af12f14550f1a368a6b2823375234a5d1c500
mar <s_jcastanon@epyc2.cluster.ucsf.bkslab.org>
2023-07-28 14:51:22 -0700

    fixing gitlab deploy script

--- backend/cartblanche/__init__.py
+++ backend/cartblanche/__init__.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 
 celery = Celery(__name__, 
-                result_backend_transport_options={'master_name': 'mymaster'},
+                result_backend_transport_options={'master_name': 'cartblanche-master'},
                 result_extended=True,
                 include=[
                            'cartblanche.data.tasks',
