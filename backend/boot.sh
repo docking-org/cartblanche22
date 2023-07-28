983bfb31cd60f7e4a1acfdc282842e5f0eb722e0
mar <s_jcastanon@epyc2.cluster.ucsf.bkslab.org>
2023-07-28 15:22:29 -0700

    fixing gitlab deploy script

--- backend/boot.sh
+++ backend/boot.sh
@@ -7,6 +7,6 @@ set -m
 rabbitmq-server -detached &
 celery -A cartblanche.run_celery.celery worker -l INFO -n worker --max-tasks-per-child=1 &
 celery -A cartblanche.run_celery.celery flower &
-gunicorn  -b :5000 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload --max-requests 3
+gunicorn  -b :5068 -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload --max-requests 3
 
 fg %1
