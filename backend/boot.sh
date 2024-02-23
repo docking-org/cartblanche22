868d21bc5f367f748683017813a02cf378009180
mar <josecastanon4@gmail.com>
2024-02-23 12:42:30 -0800

    removing logs

--- backend/boot.sh
+++ backend/boot.sh
@@ -4,8 +4,8 @@ set -o errexit
 set -o pipefail
 set -m
 
-
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=25 &
+rabbitmq-server -detached &
+celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=40 &
 celery -A cartblanche.run_celery.celery flower &
 gunicorn  -b :5068 --worker-tmp-dir /dev/shm -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload
 
