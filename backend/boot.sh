96d376d9fb591081f74661dc83fc5722bbd1f284
mar <josecastanon4@gmail.com>
2024-02-23 12:10:11 -0800

    boot script update

--- backend/boot.sh
+++ backend/boot.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o pipefail
 set -m
 
-rabbitmq-server -detached &
+
 celery -A cartblanche.run_celery.celery worker -l INFO -n worker &
 celery -A cartblanche.run_celery.celery flower &
 gunicorn  -b :5068 --worker-tmp-dir /dev/shm -w 10 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 36000 --reload
