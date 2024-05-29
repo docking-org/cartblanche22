2eba2a233161fd8f89cfd1de3f67e93719b339d4
mar <josecastanon4@gmail.com>
2024-05-29 11:52:45 -0700

    adding cache to files

--- backend/cartblanche/app.py
+++ backend/cartblanche/app.py
@@ -34,9 +34,9 @@ def serve(path):
 
 @app.after_request
 def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
+    # r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    # r.headers["Pragma"] = "no-cache"
+    # r.headers["Expires"] = "0"
     
     return r
     
