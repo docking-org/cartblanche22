09135948ecc0a8ab24b5dcc24eb42cbae1002f79
Khanh Tang <khtang015@gmail.com>
2025-10-03 23:31:03 -0700

    add logging

--- backend/boot.sh
+++ backend/boot.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -m
 
 rabbitmq-server -detached &
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=40 &
-celery -A cartblanche.run_celery.celery flower &
-gunicorn  -b :5068 --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload
+celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=30 2>&1 | tee -a /tmp/worker.log &
+celery -A cartblanche.run_celery.celery flower 2>&1 | tee -a /tmp/flower.log &
+gunicorn  -b :5068 --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
 
 fg %1
