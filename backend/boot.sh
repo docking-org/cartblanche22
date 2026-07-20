d53320ba40857c3036e52dbd42b5346ed16432e4
Khanh Tang <khtang015@gmail.com>
2026-07-20 16:50:39 -0700

    rename worker for dev

--- backend/boot.sh
+++ backend/boot.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -m
 
 
-celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=30 2>&1 | tee -a /tmp/worker.log &
+celery -A cartblanche.run_celery.celery worker -l INFO -n ${CELERY_WORKER_NAME:-worker} --concurrency=30 2>&1 | tee -a /tmp/worker.log &
 celery -A cartblanche.run_celery.celery flower 2>&1 | tee -a /tmp/flower.log &
 gunicorn  -b :${PORT:-5068} --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
 
