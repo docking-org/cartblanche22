0dc6ca9e3a4dc6f29a82e77ebc9aa152b567cedc
Khanh Tang <khtang015@gmail.com>
2026-07-20 15:41:14 -0700

    remove rabbitmq

--- backend/boot.sh
+++ backend/boot.sh
@@ -4,14 +4,6 @@ set -o errexit
 set -o pipefail
 set -m
 
-export RABBITMQ_NODENAME=rabbit@localhost
-rabbitmq-server -detached &
-
-until rabbitmqctl status > /dev/null 2>&1; do
-    echo "Waiting for RabbitMQ..."
-    sleep 2
-done
-echo "RabbitMQ is ready"
 
 celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=30 2>&1 | tee -a /tmp/worker.log &
 celery -A cartblanche.run_celery.celery flower 2>&1 | tee -a /tmp/flower.log &
