9b5821fa3295da5e2c58995bef6b16b4178a6a08
Khanh Tang <khtang015@gmail.com>
2026-07-20 12:44:31 -0700

    testing new pipeline

--- backend/boot.sh
+++ backend/boot.sh
@@ -14,6 +14,6 @@ echo "RabbitMQ is ready"
 
 celery -A cartblanche.run_celery.celery worker -l INFO -n worker --concurrency=30 2>&1 | tee -a /tmp/worker.log &
 celery -A cartblanche.run_celery.celery flower 2>&1 | tee -a /tmp/flower.log &
-gunicorn  -b :5068 --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
+gunicorn  -b :${PORT:-5068} --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
 
 fg %1
