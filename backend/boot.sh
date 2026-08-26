e7999a982cd6ccf432704b6b0fa8820ad44b32a3
Khanh Tang <khtang015@gmail.com>
2026-08-25 18:55:20 -0700

    testing devel

--- backend/boot.sh
+++ backend/boot.sh
@@ -7,6 +7,6 @@ set -m
 
 celery -A cartblanche.run_celery.celery worker -l INFO -n ${CELERY_WORKER_NAME:-worker} --concurrency=30 2>&1 | tee -a /tmp/worker.log &
 # celery -A cartblanche.run_celery.celery flower 2>&1 | tee -a /tmp/flower.log &
-gunicorn  -b :${PORT:-5068} --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
+gunicorn  -b :${PORT:-5069} --worker-tmp-dir /dev/shm -w 12 application  --timeout 36000 --reload 2>&1 | tee -a /tmp/gunicorn.log
 
 fg %1
